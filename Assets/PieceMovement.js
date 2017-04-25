5da81d93149797d534318a119b7fe6c51ac27e26
Hunter <user1@users-MacBook-Pro-2.local>
2017-04-24 23:57:30 -0500

    Added animations to clarinet pieces

---
+++ Assets/PieceMovement.js
@@ -0,0 +1,32 @@
+﻿#pragma strict
+var speed:float = 75.0;
+var x:int = 0;
+var y:int = 0;
+
+function Start () {
+
+	while(true)
+	{
+		while(x < 10)
+		{
+		transform.position.y = transform.position.y + .026;
+		yield WaitForSeconds(.07);
+		x++;
+		}
+
+		while(y < 10)
+		{
+		transform.position.y = transform.position.y - .026;
+		yield WaitForSeconds(.07);
+		y++;
+		}
+
+		x = 0;
+		y = 0;
+	}
+
+}
+
+function Update () {
+	transform.Rotate(Vector3.up, speed * Time.deltaTime);
+}
